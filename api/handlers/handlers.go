21f54490936b6e85ba3ae68f72627d58922333c7
Marcin Zub <marcin_zub@outlook.com>
2025-04-02 09:04:26 +0200

    feature: add proper api handling to the new models
    api handler functions now use newly added request and response models

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -3,17 +3,34 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/MarcinZ20/Trustify/api/models"
+	"github.com/MarcinZ20/Trustify/internal/services"
 	"github.com/gin-gonic/gin"
 )
 
 func HandleSearch(ctx *gin.Context) {
+	var requestData models.RequestData
+
+	if err := ctx.BindJSON(&requestData); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid request",
+		})
+	}
+
+	result, err := services.PerformTextAnalysis(requestData.Headline, requestData.Content, requestData.Url)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to perform the analysis",
+		})
+	}
+
 	ctx.JSON(http.StatusOK, gin.H{
-		"message": "/search endpoint",
+		"result": result,
 	})
 }
 
 func HealthCheck(ctx *gin.Context) {
-  ctx.JSON(http.StatusOK, gin.H{
-    "message": "HEALTHY",
-  })
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "HEALTHY",
+	})
 }
