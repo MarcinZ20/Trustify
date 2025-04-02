5c9a71cf9bf9276b3dc646b780809282990016f4
Marcin Zub <marcin_zub@outlook.com>
2025-04-02 09:05:17 +0200

    feature: add dummy processing function
    in place of this function there will be a call to the fastApi backend
    with AI model processing the text

---
+++ internal/services/analysis.go
@@ -0,0 +1,17 @@
+package services
+
+import (
+	"github.com/MarcinZ20/Trustify/api/models"
+)
+
+func PerformTextAnalysis(header string, content string, url string) (models.ResponseData, error) {
+	responseData := models.ResponseData{
+		Score:  78,
+		Resutl: "True",
+		Sources: []models.Source{
+			{Url: "http://example.com"}, {Url: "https://something.com"},
+		},
+	}
+
+	return responseData, nil
+}
