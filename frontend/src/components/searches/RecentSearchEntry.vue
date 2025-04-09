d7fd13b352ec9595c5404eeec3effab3ee01c5b7
Marcin Zub <marcin_zub@outlook.com>
2025-04-09 09:56:57 +0200

    feature: add recent searches container
    recent searches multi-component with child components and styling

---
+++ frontend/src/components/searches/RecentSearchEntry.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps({
+  query: String,
+  url: String,
+})
+</script>
+
+<template>
+  <div class="flex flex-row place-content-between p-2 bg-gray-200 rounded-xl">
+    <p class="text-zinc-800">{{ query }}</p>
+    <a :href="url" class="bg-zinc-800 rounded-lg text-white p-1 text-xs">Search</a>
+  </div>
+</template>
