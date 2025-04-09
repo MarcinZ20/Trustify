028d03e88fee4a74adc96e6bb700b3a6e06df902
Marcin Zub <marcin_zub@outlook.com>
2025-04-09 09:57:43 +0200

    feature: add contact page
    simple contact page according to specs

---
+++ frontend/src/views/ContactView.vue
@@ -0,0 +1,94 @@
+<script setup>
+import { ref } from 'vue'
+import { ChevronDownIcon } from '@heroicons/vue/16/solid'
+import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue'
+
+const agreed = ref(false)
+</script>
+
+<template>
+  <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8 m-2 rounded-lg">
+    <div class="mx-auto max-w-2xl text-center">
+      <h2 class="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
+        Contact us!
+      </h2>
+      <p class="mt-2 text-lg/8 text-gray-600">
+        Feel free to leave a message or a comment. Thank you!
+      </p>
+    </div>
+    <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+      <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+        <div>
+          <label for="name" class="block text-sm/6 font-semibold text-gray-900"
+            >Name</label
+          >
+          <div class="mt-2.5">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              autocomplete="given-name"
+              class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+            />
+          </div>
+        </div>
+        <div class="sm:col-span-2">
+          <label for="email" class="block text-sm/6 font-semibold text-gray-900">Email</label>
+          <div class="mt-2.5">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              autocomplete="email"
+              class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+            />
+          </div>
+        </div>
+        <div class="sm:col-span-2">
+          <label for="message" class="block text-sm/6 font-semibold text-gray-900">Message</label>
+          <div class="mt-2.5">
+            <textarea
+              name="message"
+              id="message"
+              rows="4"
+              class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+            />
+          </div>
+        </div>
+        <SwitchGroup as="div" class="flex gap-x-4 sm:col-span-2">
+          <div class="flex h-6 items-center">
+            <Switch
+              v-model="agreed"
+              :class="[
+                agreed ? 'bg-indigo-600' : 'bg-gray-200',
+                'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-gray-900/5 transition-colors duration-200 ease-in-out ring-inset focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+              ]"
+            >
+              <span class="sr-only">Agree to policies</span>
+              <span
+                aria-hidden="true"
+                :class="[
+                  agreed ? 'translate-x-3.5' : 'translate-x-0',
+                  'size-4 transform rounded-full bg-white ring-1 shadow-xs ring-gray-900/5 transition duration-200 ease-in-out',
+                ]"
+              />
+            </Switch>
+          </div>
+          <SwitchLabel class="text-sm/6 text-gray-600">
+            By selecting this, you agree to our
+            {{ ' ' }}
+            <a href="#" class="font-semibold text-indigo-600">privacy&nbsp;policy</a>.
+          </SwitchLabel>
+        </SwitchGroup>
+      </div>
+      <div class="mt-10">
+        <button
+          type="submit"
+          class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Let's talk
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
