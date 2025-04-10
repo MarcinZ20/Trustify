a34607e978996be14d58735639b95736dbe2361f
Marcin Zub <zub@student.agh.edu.pl>
2025-04-10 12:11:19 +0200

    fix: fix footer border radius
    border radius is now matching navbars border radius

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer class="bg-zinc-800 m-2 p-4 rounded-lg">
+  <footer class="bg-zinc-800 m-2 p-4 rounded-xl">
     <p class="text-white text-xs text-center">Created by Marcin Zub</p>
     <p class="text-gray-300 text-xs text-center">
       <a href="mailto:marcin_zub@outlook.com">marcin_zub@outlook.com</a>
